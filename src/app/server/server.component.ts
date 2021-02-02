b7edf40b91bd6bf3425bc5975981dc43d78bfc03
frank <frankmendezresources@gmail.com>
2021-02-02 17:25:45 +0800

    Create a new component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+import { templateSourceUrl } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+
+
+}
