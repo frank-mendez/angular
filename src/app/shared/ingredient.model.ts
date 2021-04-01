26b8f48e49b2efb336f058da052f7823314c826a
Frank Mendez <frankmendezresources@gmail.com>
2021-04-01 20:50:07 +0800

    Creating and Outputting the Shopping List

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,9 @@
+export class Ingredient {
+  public name: string;
+  public ammount: number;
+
+  constructor(name: string, amount: number) {
+    this.name = name;
+    this.ammount = amount;
+  }
+}
